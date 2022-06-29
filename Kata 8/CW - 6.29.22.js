796983d6ad8b9e6d5e816173212fde97d720c4e3
Zach Swearingen <zachswearingen95@gmail.com>
2022-06-29 10:34:15 -0500

    CW for 6.29.22

---
+++ Kata 8/CW - 6.29.22.js
@@ -0,0 +1,10 @@
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+
+function doubleChar(str) {
+    return [...str].map(s => s.repeat(2)).join('')
+  }
