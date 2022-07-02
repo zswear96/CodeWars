7805cc62998dd5ff75486b917b3212169e3d229f
Zach Swearingen <zachswearingen95@gmail.com>
2022-07-02 11:52:44 -0500

    CW for 7.2.22

---
+++ Kata 8/CW - 6.2.22.js
@@ -0,0 +1,8 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+
+
+function square(nums){
+    return nums ** 2
+    
+    }// Write the "square"-function here
