1443efe9224e11f8bfbc6933d02bb3553d6ac0c9
Zach Swearingen <zachswearingen95@gmail.com>
2022-07-11 16:30:51 -0500

    Small changes

---
+++ Kata 8/CW - 7.8.22.js
@@ -0,0 +1,11 @@
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+reverseSeq = n => {
+    let arr = []
+    for(i = 1; n >= i; i++){
+      arr.push(i)
+    }
+    return arr.reverse();
+  };
