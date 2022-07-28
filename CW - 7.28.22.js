fa951fe2c4def3a43d93e16f253671d69aabf442
Zach Swearingen <zachswearingen95@gmail.com>
2022-07-28 15:25:18 -0500

    CW for 7.28.22

---
+++ CW - 7.28.22.js
@@ -0,0 +1,9 @@
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+let a = "code"
+let b = "wa.rs"
+let name = a + b
+function result(){
+  return name
+  
+  }
