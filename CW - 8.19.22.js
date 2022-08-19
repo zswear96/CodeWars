1a00f0ae12e4fcd716a0fd9f9f9843c3f7fbd0c1
Zach Swearingen <zachswearingen95@gmail.com>
2022-08-19 18:19:24 -0500

    CW for 8.19.22

---
+++ CW - 8.19.22.js
@@ -0,0 +1,9 @@
+// Combine strings function
+// Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+// Example:
+
+// combineNames('James', 'Stevens')
+// returns:
+
+// 'James Stevens'
