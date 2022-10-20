97fc3ebf2fd083bfd1fe23434e47eaf12d68bec4
Zach Swearingen <zachswearingen95@gmail.com>
2022-10-19 19:05:50 -0500

    CW for 10.15

--- CW - 10.13.22.js
+++ CW - 10.13.22.js
@@ -2,7 +2,7 @@
 
 // Save the animals by switching them back. You will be given an array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
 
-// Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logics..
+// Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logics.
 
 // Simples!
 
