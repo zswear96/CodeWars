e26e28cb5f101d850c2fa4410dfae0b4c073ab7e
Zach Swearingen <zachswearingen95@gmail.com>
2022-12-08 19:49:20 -0600

    CW for 12.3.22

--- CW - 12.3.22.js
+++ CW - 12.3.22.js
@@ -1,4 +1,4 @@
-// Write a function that will check if two given characters are the same case...
+// Write a function that will check if two given characters are the same case..
 
 // If either of the characters is not a letter, return -1
 // If both characters are the same case, return 1
