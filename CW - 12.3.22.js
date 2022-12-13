fb179bf295075339c414e61872c556665780ed54
Zach Swearingen <zachswearingen95@gmail.com>
2022-12-12 19:51:43 -0600

    CW for 12.3.22

--- CW - 12.3.22.js
+++ CW - 12.3.22.js
@@ -14,3 +14,12 @@
 
 // '0' and '?' returns -1
 
+function sameCase(a, b){
+    if(a.toUpperCase() === a.toLowerCase() || b.toLowerCase() === b.toUpperCase()){
+       return -1
+     }else if(a === a.toLowerCase() && b === b.toLowerCase() || a === a.toUpperCase() && b === b.toUpperCase()){
+         return 1
+     }else{
+       return 0
+     }
+   }
