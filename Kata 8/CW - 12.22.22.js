2e9fc527800d2193044416181c0952e3e103913c
Zach Swearingen <zachswearingen95@gmail.com>
2022-12-26 10:09:12 -0600

    CW for 12.13.22

--- Kata 8/CW - 12.22.22.js
+++ Kata 8/CW - 12.22.22.js
@@ -2,3 +2,13 @@
 
 // Note: Empty arrays should return 0..
 
+function find_average(array) {
+    if(array.length >= 1){
+        // if the array length is greater or equal to 1 then run reduce function
+     let add = array.reduce(
+       (acc, curr) => acc + curr, 0)
+    return(add/array.length)
+     }else{
+       return 0
+     }
+   }
