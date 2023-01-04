ed8e844ec1b86dba87000a981a6878826884dec8
Zach Swearingen <zachswearingen95@gmail.com>
2023-01-04 15:25:21 -0600

    CW for 12.13.22

--- Kata 8/CW - 1.3.23.js
+++ Kata 8/CW - 1.3.23.js
@@ -2,3 +2,8 @@
 
 // In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
 
+const well = x => {
+    const good_count = x.filter(x => x == 'good').length;
+    return good_count < 1 ? 'Fail!' : 
+           good_count < 3 ? 'Publish!' : 'I smell a series!';
+  }
