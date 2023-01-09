9bb2352ec358b103c697f4f1061675a8543a6ab1
Zach Swearingen <zachswearingen95@gmail.com>
2023-01-08 21:12:59 -0600

    CW for 12.13.22

--- Kata 8/CW - 1.5.23.js
+++ Kata 8/CW - 1.5.23.js
@@ -6,7 +6,7 @@
 
 // For an example list of 1, 3, 5, 7
 
-// 1. Add all of the numbers...
+// 1. Add all of the numbers....
 
 // 1+3+5+7 = 16
 // 2. Divide by the number of values in the list. In this example there are 4 numbers in the list.
