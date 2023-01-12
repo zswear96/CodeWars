21c1397d109a4e5b3fdaf977f2dc6d526f777d7e
Zach Swearingen <zachswearingen95@gmail.com>
2023-01-12 10:05:11 -0600

    CW for 1.11.23

--- Kata 8/CW - 2.19.23.js
+++ Kata 8/CW - 2.19.23.js
@@ -13,8 +13,10 @@
 // +---++---++---++---+         +---++---++---++---+
 // |   ||   ||   ||   |         |   ||   ||   ||   |
 // +---++---++---++---+         +---++---++---++---+
-// Given the initial configuration of the cubes in the box, find out how many cubes are in each of the n columns after Bob switches the gravity.
+// Given the initial configuration of the cubes in the box, find out how many cubes are in each of the n columns after Bob switches the gravity..
 
 // Examples (input -> output:
 // * 'R', [3, 2, 1, 2]      ->  [1, 2, 2, 3]
 // * 'L', [1, 4, 5, 3, 5 ]  ->  [5, 5, 4, 3, 1]
+
+
