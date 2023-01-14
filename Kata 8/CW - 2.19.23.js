e520b663967197e2520709bbe24bedfe1e647412
Zach Swearingen <zachswearingen95@gmail.com>
2023-01-14 10:05:59 -0600

    CW for 1.11.23

--- Kata 8/CW - 2.19.23.js
+++ Kata 8/CW - 2.19.23.js
@@ -19,4 +19,10 @@
 // * 'R', [3, 2, 1, 2]      ->  [1, 2, 2, 3]
 // * 'L', [1, 4, 5, 3, 5 ]  ->  [5, 5, 4, 3, 1]
 
-
+const flip=(d, a)=>{
+    if(d ==="R"){
+      return a.sort((a,b)=> a - b)
+    }else if (d === "L"){
+      return a.sort((a,b)=> b - a)
+    }
+   }
