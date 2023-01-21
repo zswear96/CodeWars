3a043d87b36fdf63a47b76d996dce86a07f84e0f
Zach Swearingen <zachswearingen95@gmail.com>
2023-01-21 17:06:05 -0600

    CW for 10.20

--- CW - 10.20.22.js
+++ CW - 10.20.22.js
@@ -2,5 +2,5 @@
 
 // You only need to worry about english grammar rules for this kata, where anything that isn't singular (one of something), it is plural (not one of something).
 
-// All values will be positive integers or floats, or zero.
+// All values will be positive integers or floats, or zero..
 
