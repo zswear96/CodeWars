32b294c29df1861cb23645b77b22960072c4c62b
Zach Swearingen <zachswearingen95@gmail.com>
2023-02-04 18:40:39 -0600

    Cw 2.1.23

--- Kata 8/CW - 2.21.23.js
+++ Kata 8/CW - 2.21.23.js
@@ -6,7 +6,7 @@
 // "you will win" -->["you 3", "will 4", "win 3"]
 // Your task is to write a function that takes a String and returns an Array/list with the length of each word added to each element .
 
-// Note: String will have at least one element; words will always be separated by a space..
+// Note: String will have at least one element; words will always be separated by a space...
 
 function addLength(str) {
     return str.split(' ').map((element) => `${element} ${element.length}`)
