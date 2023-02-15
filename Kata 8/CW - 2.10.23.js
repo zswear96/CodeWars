a0ad6c2f27eda42e125a9d21b19c45946925e74d
Zach Swearingen <zachswearingen95@gmail.com>
2023-02-15 14:59:17 -0600

    Cw for 2.5.23

--- Kata 8/CW - 2.10.23.js
+++ Kata 8/CW - 2.10.23.js
@@ -1,4 +1,4 @@
-// Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length )..
+// Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
 
 // Hint for R users:
 
