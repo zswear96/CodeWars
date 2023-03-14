4f523ef59745291056d20d4119b8140b89d3be75
Zach Swearingen <zachswearingen95@gmail.com>
2023-03-14 11:44:40 -0500

    CW for 3.13.23

--- Kata 8/CW - 3.13.23.js
+++ Kata 8/CW - 3.13.23.js
@@ -1,4 +1,4 @@
-// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+// Complete the square sum function so that it squares each number passed into it and then sums the results together..
 
 // For example, for [1, 2, 2] it should return 9 because 
 // 1
