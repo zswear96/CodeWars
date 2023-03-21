ec4cb39192651cb9b31d28d7c23c2ac1db957aad
Zach Swearingen <zachswearingen95@gmail.com>
2023-03-21 15:47:18 -0500

    CW for 3.15.23

--- Kata 8/CW - 3.21.23.js
+++ Kata 8/CW - 3.21.23.js
@@ -1,4 +1,4 @@
-// Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+// Return a new array consisting of elements which are multiple of their own index in input array (length > 1)
 
 // Some cases:
 // [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
