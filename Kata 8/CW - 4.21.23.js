28e90c11985f221c5306ff0d0853bc37b638eabf
Zach Swearingen <zachswearingen95@gmail.com>
2023-04-07 08:59:05 -0500

    Cw for 4.6.23

--- Kata 8/CW - 4.21.23.js
+++ Kata 8/CW - 4.21.23.js
@@ -12,11 +12,9 @@
 // Each weekday in the stairs array is an array.
 
 // sunday = [6737, 7244, 5776, 9826, 7057, 9247, 5842, 5484, 6543, 5153, 6832, 8274, 7148, 6152, 5940, 8040, 9174, 7555, 7682, 5252, 8793, 8837, 7320, 8478, 6063, 5751, 9716, 5085, 7315, 7859, 6628, 5425, 6331, 7097, 6249, 8381, 5936, 8496, 6934, 8347, 7036, 6421, 6510, 5821, 8602, 5312, 7836, 8032, 9871, 5990, 6309, 7825]
-<<<<<<< HEAD
-// Your function should return the 20 year estimate of the stairs climbed using the formula above.....
-=======
+
 // Your function should return the 20 year estimate of the stairs climbed using the formula above....
->>>>>>> 051c0a973e5de8afaa61e60e4c1147d990cbdf0d
+
 
 function stairsIn20(s){
     return s.reduce((a,b) => a.concat(b)).reduce((a,b) => a + b) * 20;
